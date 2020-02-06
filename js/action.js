a24e7523c9edf96bc89a2ed40839039c9b93d709
EduardoAGalvao <eduardo.aug.galvao@gmail.com>
2020-02-05 23:04:35 -0300

    Adicionando imagens novas, alterações no JS, alterações no index e na folha de estilo - 05/02/2020

--- js/action.js
+++ js/action.js
@@ -13,5 +13,38 @@ $(document).ready(function() {
     
   }
   
-});
+  $(window).scroll(function(){ // função para pegar o movimento do scroll (barra de rolagem)
+    var top = $(window).scrollTop(); // aqui vc pega a posição da página
 
+    if(top > 480){ // verifica a posição da página
+
+        $("#seta").show(); // aqui vc aplica o fade no menu
+    }else{
+        $("#seta").hide();
+    }
+  }); 
+  
+  //EFEITO DE ANCORA NA NAVEGAÇÃO EM LANDPAGES
+  function filterPath(string) {
+      return string
+        .replace(/^\//,'')
+        .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
+        .replace(/\/$/,'');
+    }
+    $('a[href*=\\#]').each(function() {
+      if ( filterPath(location.pathname) == filterPath(this.pathname)
+      && location.hostname == this.hostname
+      && this.hash.replace(/#/,'') ) {
+        var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+        var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+         if ($target) {
+           var targetOffset = $target.offset().top - 100;
+           $(this).click(function() {
+             $('html, body').animate({scrollTop: targetOffset}, 1000);
+             return false;
+           });
+        }
+      }
+    });
+  
+});
